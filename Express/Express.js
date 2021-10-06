87d269ec1fa8f6749d304da674fd04cc30aecc6b
venkateshprasad598 <venkateshprasad598@gmail.com>
2021-10-06 15:30:32 +0530

    Ad

--- Express/Express.js
+++ Express/Express.js
@@ -1,14 +1,15 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-app.get("/", (req, res) => {
-  res.status(200).send("Welcome to my first Express program");
-});
-app.get("/about", (req, res) => {
-  res.status(200).send("About my page");
-});
-app.all("*", (req, res) => {
-  res.status(404).send("Request not found");
-});
+// Store All static fles, for example styling pages, funcnality pages which are related to sendFile.
+app.use(express.static("./public"));
 
-app.listen(5000);
-//ok
+app.get("/about", (req, res) => {
+  res.status(200).send("My About Page");
+});
+app.get("*", (req, res) => {
+  res.status(404).send("Response not found");
+});
+app.listen(3000, () => {
+  console.log("Server is listening at port 5000...");
+});

5f75748657b14b1066fc9313a1b8695c4ad2c9d1
venkateshprasad598 <venkateshprasad598@gmail.com>
2021-10-07 18:20:05 +0530

    Add

--- Express/Express.js
+++ Express/Express.js
@@ -1,14 +1,5 @@
 const express = require("express");
 const app = express();
-const about = require("./About");
-const logger = require("./data/logger");
-
-app.use(logger);
-app.use("/about", about);
-
-app.get("/", (req, res) => {
-  res.status(200).json("Hello World");
-});
-app.listen(5000, () => {
-  console.log("server is listening at port 5000...");
-});
+const tasks = require("./data/tasks");
+app.use("/api/v1/task", tasks);
+app.listen(5000);

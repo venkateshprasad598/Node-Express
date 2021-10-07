606ff1114697e4a728fd2b75bb00dd5f7b51009c
venkateshprasad598 <venkateshprasad598@gmail.com>
2021-10-07 18:25:31 +0530

    Add

--- Express/Express.js
+++ Express/Express.js
@@ -2,4 +2,4 @@ const express = require("express");
 const app = express();
 const tasks = require("./data/tasks");
 app.use("/api/v1/task", tasks);
-app.listen(5000);
+app.listen(5000, console.log("server is listing at 5000"));
